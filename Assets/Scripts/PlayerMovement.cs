a562a2c026b8b229bde3449e7bdf9c3d0d437a07
ArtemDadashjants <tfastgame@gmail.com>
2022-05-24 11:45:53 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float Speed = 0f;
+    [SerializeField] private Rigidbody2D rb = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Debug.Log(x);
+        if( x >= 0.1f)
+        {
+            rb.AddForce(new Vector2(x*Speed*Time.deltaTime,0), ForceMode2D.Impulse);
+        }
+        else if( x <= -0.1f)
+        {
+            rb.AddForce(new Vector2(x*Speed*Time.deltaTime,0), ForceMode2D.Impulse);
+        }
+    }
+}
